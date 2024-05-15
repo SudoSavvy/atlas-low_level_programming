b421421c942467f5400e00e417e085b021b2bc28
SudoSavvy <donovanjheape@gmail.com>
2024-05-15 15:51:23 +0000

    C programming

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
-	puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    /* Print the desired string with the opening double quote */
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return (0);
 }
