4bbf70f347469138317744cd2bce811567974374
SudoSavvy <donovanjheape@gmail.com>
2024-05-15 16:35:30 +0000

    C programming

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main(void){
 
-/* insert comment funny haha joke */
+/*
+  
+ * insert comment funny haha joke 
+
+*/
 
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
@@ -9,5 +13,5 @@ printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
-return 0;
+return (0);
 }
