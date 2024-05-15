18ec1124ae50ea76db608c4c7cc7ef447b3fd17e
SudoSavvy <donovanjheape@gmail.com>
2024-05-15 16:23:42 +0000

    C programming

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-int main(void){
-	
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a specific message using printf.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 
-return (0);
+return 0;
 }
