6b633ea2a206aab2e9442173ded420811e55145b
SudoSavvy <donovanjheape@gmail.com>
2024-05-15 18:32:07 +0000

    C programming

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,17 +14,17 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-printf("%d is positive\n", n)
+printf("%d is positive\n", n);
 }
 
 else if (n == 0)
 {
-printf("%d is zero\n")
+printf("%d is zero\n");
 }
 
 else 
 {
-printf("%d is negative\n")
+printf("%d is negative\n");
 }
 
 return (0);
