278ba353d3f6667b161f6035c0785a4102476a7f
SudoSavvy <donovanjheape@gmail.com>
2024-05-20 23:38:33 +0000

    More scripting in c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,25 +1,21 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - the entry point to my code
+ * main - Prints "_putchar" followed by a new line.
  *
- * Description: this code should print '_putchar' followed by a new line
- *
- * Return: Always 0 (success)
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	char *c;
-	int i;
-
-	c = "_putchar";
-	for (i = 0; c[i] != '\0'; i++)
-	{
-		putchar(c[i]);
-	}
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
