8396ecbad8eda38f2ef1e854fff09540b63f68b9
SudoSavvy <donovanjheape@gmail.com>
2024-05-22 20:57:08 +0000

    C functions and loops

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -6,10 +6,7 @@
  *
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isupper(int c)
-{
-    if (c >= 'A' && c <= 'Z')
-        return 1;
-    else
-        return 0;
-}
+int _putchat(char c);
+int _isupper(int c);
+
+#endif
