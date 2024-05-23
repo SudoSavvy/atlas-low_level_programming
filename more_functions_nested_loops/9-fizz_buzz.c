72689b713c0f6c2c284f2587270e868e3a519a84
SudoSavvy <donovanjheape@gmail.com>
2024-05-23 21:06:33 +0000

    c programming with functions

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,38 +7,30 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-    int num;
+	int num;
 
-    for (num = 1; num <= 100; num++)
-    {
-      
-	     if (num % 3 == 0 && num % 5 == 0)
-        
-		    printf("FizzBuzz");
-      
-     	     else if (num % 3 == 0)
-      
-	     	     printf("Fizz");
-      
-      	     else if (num % 5 == 0)
-        
-	      	     printf("Buzz");
-      
-      	     else
-      
-	             printf("%d", num);
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
 
-      	     if (num == 100)
-	      	     continue;
-       	     printf(" ");
-    
-    }
-  
-    printf("\n");
+		else if ((num % 3) == 0)
+			printf("Fizz");
 
-    return (0);
+		else if ((num % 5) == 0)
+			printf("Buzz");
 
+		else
+			printf("%d", num);
+
+		if (num == 100)
+			continue;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
 }
