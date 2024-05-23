084449c650a45999fd446bf0a3e4e02bfc94f1e5
SudoSavvy <donovanjheape@gmail.com>
2024-05-23 19:07:22 +0000

    c programming with functions

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * functons that pruints numbers 0-9 excluding 2 and four.
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num !=4)
+			_putchar((num% 10) + '0');
+	}
+
+	_putchar('\n');
+}
