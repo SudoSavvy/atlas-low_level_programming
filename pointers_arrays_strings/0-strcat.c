f12f5bba8aac5003188d0dc2895bc0dfca1af354
SudoSavvy <donovanjheape@gmail.com>
2024-05-29 16:16:24 -0500

    Commit_message_here

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,22 @@
 #include "main.h"
 #include <stdio.h>
+/*
+*
+*/
 
-char *_strcat(char *dest, const char *src) {
+char *_strcat(char *dest, char *src) {
     char *dest_end = dest;
 
-    // Find the end of the dest string
     while (*dest_end != '\0') {
         dest_end++;
     }
 
-    // Copy src to the end of dest
     while (*src != '\0') {
         *dest_end = *src;
         dest_end++;
         src++;
     }
 
-    // Null-terminate the resulting string
     *dest_end = '\0';
 
     return dest;
@@ -27,7 +27,7 @@ int main(void) {
     char src[] = "world!";
 
     _strcat(dest, src);
-    printf("%s\n", dest);  // Output should be "Hello, world!"
+    printf("%s\n", dest);
 
     return 0;
 }
