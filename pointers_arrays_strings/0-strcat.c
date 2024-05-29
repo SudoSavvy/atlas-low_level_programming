3b178ce0a2dbd2e0e9454eb58b67940866af355e
SudoSavvy <donovanjheape@gmail.com>
2024-05-29 16:56:50 -0500

    Commit_message_here

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -6,17 +6,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i = 0;
-int n = 0;
+    int i = 0;
+    int n = 0;
+            
+    for (i = 0; dest[i] != '\0'; i++)
+    {
+    }
 
-            for (i = 0; dest[i] != '\0'; i++)
-            {}
+    for (n = 0; src[n] != '\0'; n++, i++)
+    {
+        dest[i] = src[n];
+    }
 
-            for (n = 0; src[n] != '\0'; n++, i++)
-            {
-                dest[i] = src[n];
-            }
-
-        return (dest);
-
+    return (dest);
 }
