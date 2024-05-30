ca6f9a151c7ddf7ebcf09d14beffdb89661f7473
SudoSavvy <donovanjheape@gmail.com>
2024-05-30 14:06:58 -0500

    Continuing Pointer and array c programming

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -7,15 +7,15 @@
  * @n: number of bytes filled
  *
  * Return: pointer
-*/
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+    unsigned int i;
 
-	for (i = 0; s[i] == '\0' && i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
+    for (i = 0; i < n; i++)
+    {
+        s[i] = b;
+    }
+    return (s);
 }
