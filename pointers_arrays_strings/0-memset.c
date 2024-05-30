6333aad454e8f4600213b89dea4bca8b99612650
SudoSavvy <donovanjheape@gmail.com>
2024-05-30 14:01:49 -0500

    Continuing Pointer and array c programming

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * _memset - Fills memory with a constant byte
- * @s: Pointer to the memory area to be filled
- * @b: The constant byte to fill the memory area with
- * @n: The number of bytes to be filled
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: number of bytes filled
  *
- * Return: Pointer to the memory area s
- */
+ * Return: pointer
+*/
 
-char *_memset(char *s, char b, unsigned int n) {
-    char *ptr = s;
-    while (n-- > 0) {
-        *ptr++ = b;
-    }
-    return s;
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] == '\0' && i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
