77958618722761544f7148b939f7d4b86798830e
SudoSavvy <donovanjheape@gmail.com>
2024-05-29 19:15:10 -0500

    Atlas c programming

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two strings but it can only us n amount of bytes
+ * _strncat - Concatenates two strings using at most n bytes from src
  * @dest: The destination string
  * @src: The source string
+ * @n: The maximum number of bytes to be used from src
  *
  * Return: Pointer to the destination string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int x = 0;
+    int i = 0;
+    int x = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
+    for (i = 0; dest[i] != '\0'; i++)
+    {
+    }
 
-	while (src[x] != '\0' && x < n)
-	{
-		dest[i] = src[x];
+    while (src[x] != '\0' && x < n)
+    {
+        dest[i] = src[x];
         x++;
         i++;
-
-	}
+    }
     dest[i] = '\0';
 
-	return (dest);
+    return (dest);
 }
