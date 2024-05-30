40f3dd509003884645b36ddeb72985ef08633201
SudoSavvy <donovanjheape@gmail.com>
2024-05-29 19:50:57 -0500

    Atlas c programming

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@
  * Return: Integer less than, equal to, or greater than zero if s1 is found,
  * respectively, to be less than, to match, or be greater than s2.
  */
-int _strcmp(const char *s1, const char *s2)
+int _strcmp(char *s1, char *s2)
 {
     int i = 0;
 
