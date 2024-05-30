17ec045d7357d351c2cc420bd32f8aee8c801bf5
SudoSavvy <donovanjheape@gmail.com>
2024-05-29 19:50:21 -0500

    Atlas c programming

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,23 +2,21 @@
 
 /**
  * _strcmp - Compares two strings
- * @s1: The first string
- * @s2: The second string
- * 
- * Return: Difference in ASCII values of first non-matching characters
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Integer less than, equal to, or greater than zero if s1 is found,
+ * respectively, to be less than, to match, or be greater than s2.
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(const char *s1, const char *s2)
 {
     int i = 0;
 
     while (s1[i] != '\0' && s2[i] != '\0')
     {
         if (s1[i] != s2[i])
-        {
             return (s1[i] - s2[i]);
-        }
         i++;
     }
-
     return (s1[i] - s2[i]);
 }
