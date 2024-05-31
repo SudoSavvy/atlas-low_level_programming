5fde0362fdbc1f66ae3364a3964a6197613bac7c
SudoSavvy <donovanjheape@gmail.com>
2024-05-31 11:33:05 -0500

    finishing up this sprint

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,7 +8,7 @@
  * Return: the number of bytes in the initial segment of s
  * which consist only of bytes from accept
  */
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 	unsigned int count = 0;
