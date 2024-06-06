9e2fdf82fdaeffebe8624d469f2c12c253cf8f2f
SudoSavvy <donovanjheape@gmail.com>
2024-06-05 22:27:23 -0500

    Fun recursion coding time

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,13 +7,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        
-        return;
-        
-    }
-
-    _print_rev_recursion(s + 1);
-    write(1, s, 1);
+if (*s == '\0')
+{       
+return;
+}
+_print_rev_recursion(s + 1);
+write(1, s, 1);
 }
