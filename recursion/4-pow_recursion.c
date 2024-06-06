e012f6d2220ab94791e1226ea573696abf2f2901
SudoSavvy <donovanjheape@gmail.com>
2024-06-06 11:38:50 -0500

    Day 2 of programming in c with recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - Returns the value of x raised to the power of y.
  * @x: base integer.
  *@y: raised integer.
- *  
+ *
  * return: int
 */
 
@@ -14,7 +14,7 @@ if (y < 0)
 return (-1);
 
 if (y == 0)
-return(1);
+return (1);
 
 y--;
 return (x * (_pow_recursion(x, y)));
