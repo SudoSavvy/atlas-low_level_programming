1fc140b49d10c8db034e1ca1e2fa42d32fe74715
SudoSavvy <donovanjheape@gmail.com>
2024-06-06 14:40:48 -0500

    Day 2 of programming in c with recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
 /**
- * is_prime_number - function returns 1 if the number is a prime number, and a 0 if anything else.
- * @n: The input number.
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to check.
  *
- * Return: 0
-*/
+ * Return: 1 if @n is prime, 0 otherwise.
+ */
 
-int is_prime_number(int n) {
-    if (n <= 1) {
-        return 0;
-    }
+int is_prime_number(int n)
+{
     int i;
-    for (i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return 0;
+
+    if (n <= 1)
+    {
+        return (0);
+    }
+
+    for (i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return (0);
         }
     }
-    return 1;
+
+    return (1);
 }
