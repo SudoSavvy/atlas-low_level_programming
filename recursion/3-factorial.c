01d303d6d5f0f55b8e5f9b019bcbf6e98428a0a3
SudoSavvy <donovanjheape@gmail.com>
2024-06-06 10:53:54 -0500

    Day 2 of programming in c with recursion

---
+++ recursion/3-factorial.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * 
+ * 
+*/
+
+int factorial(int n)
+{
+    if (n < 0)
+    {
+        return (-1);
+    }
+    if (n == 0)
+    {
+        return (1);
+    }
+    return (n * factorial(n - 1));
+}
+
+int main(void)
+{
+    int num = 5;
+    int result = factorial(num);
+
+    if (result == -1)
+    {
+        printf("Error: factorial of a negative number doesn't exist.\n")
+    }
+    else
+    {
+        printf("Factorial of a %d is %d\n", num, result);
+    }
+
+    return(0);
+}
