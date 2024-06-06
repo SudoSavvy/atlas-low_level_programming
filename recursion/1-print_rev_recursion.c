d73bec9deee605b1c8feb0aa22538319e5439147
SudoSavvy <donovanjheape@gmail.com>
2024-06-05 21:57:21 -0500

    Fun recursion coding time

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * 
+ * 
+*/
+
+void _print_rev_recursion(char *s)
+{
+    if (*s == '0')
+    {
+        return; 
+    }
+
+    _print_rev_recursion(s + 1);
+    write(1, s ,1);
+}
--- recursion/main_0.c
+++ recursion/main_0.c
@@ -10,3 +10,12 @@ int main()
     _puts_recursion(str);
     return (0);
 }
+
+int main(void)
+{
+    char str[] = "hello";
+    _print_rev_recursion(str);
+    write(1, '\n' ,1);
+
+    return (0);
+}
