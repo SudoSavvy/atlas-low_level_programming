a8004336cce3ac838282ed3ac44cd84a55862c6f
SudoSavvy <donovanjheape@gmail.com>
2024-06-06 14:42:16 -0500

    Day 2 of programming in c with recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -19,8 +19,8 @@ int is_divisible(int n, int divisor)
 int is_prime_number(int n)
 {
     if (n <= 1)
-        return 0; // 0 and 1 are not prime numbers
+        return 0;
     if (n == 2)
-        return 1; // 2 is a prime number
+        return 1;
     return !is_divisible(n, 2);
 }
