b77bc8347583d37d0f83f6e6c912add540cc6d24
SudoSavvy <donovanjheape@gmail.com>
2024-06-05 20:16:45 -0500

    Fun recursion coding time

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,20 +1,18 @@
 #include "main.h"
 #include <unistd.h>
+
 /**
- * Desc: _puts_recursion prints a string then a new line.
- * @s: the string
-*/
-
- void _puts_recursion(char *s)
+ * _puts_recursion - prints a string followed by a new line
+ * @s: the string to print
+ */
+void _puts_recursion(char *s)
 {
-if (*s == '\0')
-{ 
-    write(1,"\n", 1);
-    return;
+    if (*s == '\0')
+    {
+        write(1, "\n", 1);
+        return;
+    }
 
-}
-
-write(1, s, 1);
-
-_puts_recursion(s +1);
+    write(1, s, 1);
+    _puts_recursion(s + 1);
 }
