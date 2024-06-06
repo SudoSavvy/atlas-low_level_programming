422188325f4b2407056bf3ab8e492a2dc3974c6b
SudoSavvy <donovanjheape@gmail.com>
2024-06-05 22:21:22 -0500

    Fun recursion coding time

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -2,17 +2,16 @@
 #include <unistd.h>
 
 /**
- * 
- * 
-*/
-
+ * _print_rev_recursion - prints a string in reverse using recursion
+ * @s: the string to print
+ */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '0')
+    if (*s == '\0')
     {
-        return; 
+        return;
     }
 
     _print_rev_recursion(s + 1);
-    write(1, s ,1);
+    write(1, s, 1);
 }
