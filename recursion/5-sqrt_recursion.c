bb6f11b6cab63bb5e75babd998bc854323f15d84
SudoSavvy <donovanjheape@gmail.com>
2024-06-06 13:27:20 -0500

    Day 2 of programming in c with recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,15 +1,33 @@
 #include "main.h"
 
 /**
- *  _sqrt_recursion - finds the natural square root of a number recursively.
- * @n: The number.
+ *  _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to find the square root of.
  * 
- * Return: int
+ * Return: The natural square root, or -1 if no natural square root exists. 
 */
-
 int _sqrt_recursion(int n)
 {
-if (n == 0)
-return (-1);
-return(n * _sqrt_recursion(n - 1));
+    if (n < 0)
+    {
+        return(-1);
+    }
+    if (n == 0 || n == 1)
+    {
+        return (n);
+    }
+    return (_sqrt_recursion_helper(n, 1));
+}
+
+int _sqrt_recursion_helper(int n, int guess)
+{
+    if (guess * guess == n)
+    {
+        return guess;
+    }
+    if (guess * guess > n)
+    {
+        return -1;
+    }
+    return _sqrt_recursion_helper(n, guess + 1);
 }
