948ff8d67c12b756b46bc89679ee431d0cdc18ed
SudoSavvy <donovanjheape@gmail.com>
2024-06-07 16:54:04 -0500

    Static libraries day

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of pointers to the arguments
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(int argc, char *argv[])
 {
