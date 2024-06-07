62783c2ce254c597437baf6fb64d79061bf3ae39
SudoSavvy <donovanjheape@gmail.com>
2024-06-07 17:35:31 -0500

    Static libraries day

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ * @argc: Number of arguments passed to the proram.
+ * @argv: Array of pointers to the arguments.
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(int argc, cahr *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i ++)
+{
+printd("%s\n", argv[i]);
+}
+
+return (0);
+}
