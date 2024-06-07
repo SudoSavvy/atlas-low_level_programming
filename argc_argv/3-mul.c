6c62ad3f54fdde128b926fbb031e1ae6f563ccf6
SudoSavvy <donovanjheape@gmail.com>
2024-06-07 17:44:26 -0500

    Static libraries day

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point for the program.
+ * @argc: NUmber of arguments passed to the prorgram.
+ * @argv: Array of pointers to the arguments
+ * 
+ * Return: 0 on success, 1 on error.
+*/
+int main(argc, char *argv[])
+{
+int num1, num2, result;
+if (argc != 3)
+{
+printf("Error\n");
+return(1)
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+
+result = num1 * num2;
+
+printf("%d\n", result);
+
+return (0);
+}
