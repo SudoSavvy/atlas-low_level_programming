ed54938835d222512b5635d1d12f74f7c0220961
SudoSavvy <donovanjheape@gmail.com>
2024-06-12 14:38:42 -0500

    Malloc progrmming YIPPEEE

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -7,35 +7,35 @@
  *           which contains a copy of the string given as a parameter.
  *
  * @str: The input string to duplicate.
- * 
+ *
  * Return: A pointer to the duplicated string on success.
  *         NULL if str is NULL or if insufficient memory was available.
  */
 char *_strdup(char *str)
 {
-    unsigned int length = 0;
-    char *copy;
-    unsigned int i;
+	unsigned int length = 0;
+	char *copy;
+	unsigned int i;
 
-    /* Check if 'str' is NULL */
-    if (str == NULL)
-        return (NULL);
+	/* Check if 'str' is NULL */
+	if (str == NULL)
+		return (NULL);
 
-    /* Determine the length of 'str' */
-    while (str[length] != '\0')
-        length++;
+	/* Determine the length of 'str' */
+	while (str[length] != '\0')
+		length++;
 
-    /* Allocate memory for the new string */
-    copy = (char *)malloc((length + 1) * sizeof(char));
+	/* Allocate memory for the new string */
+	copy = (char *)malloc((length + 1) * sizeof(char));
 
-    /* Check if memory allocation was successful */
-    if (copy == NULL)
-        return (NULL);
+	/* Check if memory allocation was successful */
+	if (copy == NULL)
+		return (NULL);
 
-    /* Copy the input string */
-    for (i = 0; i <= length; i++)
-        copy[i] = str[i];
+	/* Copy the input string */
+	for (i = 0; i <= length; i++)
+		copy[i] = str[i];
 
-    /* Return the duplicated string */
-    return (copy);
+	/* Return the duplicated string */
+	return (copy);
 }
