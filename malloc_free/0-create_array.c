a3774529634f6d216ce895a37a264cbb75b72f91
SudoSavvy <donovanjheape@gmail.com>
2024-06-12 11:26:04 -0500

    Malloc progrmming YIPPEEE

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+    unsigned int i = 0
+    char *array = NULL
+
+    /** Checks if size is 0 */
+    if (size == 0)
+        return (NULL);
+
+    /** Allocate memory for array */
+    array = malloc(sizeof(char));
+
+    /**check if malloc was successful */
+    if (array = NULL)
+        return (NULL);
+    
+    /**initialise the array with the character c */
+    for (i = 0; i < size; i++
+    array[i] = c;
+
+    return (array);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * HEader file containting all prototypes for malloc_free
+ */
+
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c);
+
+
+#endif
