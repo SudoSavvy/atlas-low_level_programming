b1aeaf321115e38805a3ee6bed1bc69d85382614
SudoSavvy <donovanjheape@gmail.com>
2024-06-12 15:36:40 -0500

    Malloc progrmming YIPPEEE

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -12,11 +12,12 @@
  */
 void free_grid(int **grid, int height)
 {
+    int i;
     if (grid == NULL)
         return;
 
     /** Free each row of the grid */
-    for (int i = 0; i < height; i++)
+    for (i = 0; i < height; i++)
     {
         free(grid[i]);
     }
