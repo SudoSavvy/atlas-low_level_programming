a8e12065dbfb9c4479cdacb8b221aea1b03f9e73
SudoSavvy <donovanjheape@gmail.com>
2024-06-13 11:58:43 -0500

    More malloc programming :D

---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Yet another malloc programming  directory.
---
+++ more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/** Header file containing all prototypes for the directory more_malloc_free*/
+
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b);
+
+#endif
