6e0235879149320e9b2bc76935af80a7d327551a
SudoSavvy <donovanjheape@gmail.com>
2024-06-13 13:46:53 -0500

    More malloc programming :D

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -10,19 +10,19 @@
  */
 int *array_range(int min, int max)
 {
-    int *arr;
-    int size, i;
+int *arr;
+int size, i;
 
-    if (min > max)
-        return (NULL);
+if (min > max)
+return (NULL);
 
-    size = max - min + 1;
-    arr = (int *)malloc(size * sizeof(int));
-    if (arr == NULL)
-        return NULL;
+size = max - min + 1;
+arr = (int *)malloc(size * sizeof(int));
+if (arr == NULL)
+return (NULL);
 
-    for (i = 0; i < size; i++)
-        arr[i] = min + i;
+for (i = 0; i < size; i++)
+arr[i] = min + i;
 
-    return (arr);
+return (arr);
 }
