3d0471d09d2ccdcbd3ddd0ac8c38a61a0189ef2e
SudoSavvy <donovanjheape@gmail.com>
2024-06-14 10:27:30 -0500

    Preprocessor programming in c

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "abs.h"
+
+int main(void)
+{
+int a = -5;
+int b = 3;
+printf("ABS(%d) = %d\n", a, ABS(A));
+printf("ABS(%d) = %d\n", b, ABS(b));
+printf("ABS(%d) = %d\n", c, ABS(c));
+
+return (0);
+}
