e2c0582f59f53917ee372a6dfa098290994f3292
SudoSavvy <donovanjheape@gmail.com>
2024-06-19 15:22:24 -0500

    struct and typedef

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,10 +8,7 @@
 void print_dog(struct dog *d)
 {
     if (d == NULL)
-    {
-        printf("NULL\n");
         return;
-    }
 
     printf("Name: %s\n", d->name ? d->name : "(nil)");
     printf("Age: %.6f\n", d->age);
