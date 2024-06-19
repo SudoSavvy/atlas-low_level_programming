a4b7c0389256d39dbfc18ebbef6be5fa36fc78cd
SudoSavvy <donovanjheape@gmail.com>
2024-06-19 15:39:17 -0500

    struct and typedef

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,6 +13,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
+
 if (name == NULL || owner == NULL)
 return NULL;
 
@@ -20,22 +21,24 @@ d = malloc(sizeof(dog_t));
 if (d == NULL)
 return NULL;
 
-d->name = strdup(name);
+d->name = malloc(strlen(name) + 1);
 if (d->name == NULL)
 {
 free(d);
-return (NULL);
+return NULL;
 }
+strcpy(d->name, name);
 
-d->owner = strdup(owner);
+d->owner = malloc(strlen(owner) + 1);
 if (d->owner == NULL)
 {
 free(d->name);
 free(d);
-return (NULL);
+return NULL;
 }
+strcpy(d->owner, owner);
 
 d->age = age;
 
-return (d);
+return d;
 }
