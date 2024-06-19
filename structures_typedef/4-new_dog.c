42bc9b78caf81bde6acdd4e72832b669683ec769
SudoSavvy <donovanjheape@gmail.com>
2024-06-19 15:36:33 -0500

    struct and typedef

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog, or NULL if it fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *d;
+if (name == NULL || owner == NULL)
+return NULL;
+
+d = malloc(sizeof(dog_t));
+if (d == NULL)
+return NULL;
+
+d->name = strdup(name);
+if (d->name == NULL)
+{
+free(d);
+return (NULL);
+}
+
+d->owner = strdup(owner);
+if (d->owner == NULL)
+{
+free(d->name);
+free(d);
+return (NULL);
+}
+
+d->age = age;
+
+return (d);
+}
