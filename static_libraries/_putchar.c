a9dc1efcdd2b8aa86b90de64421082054535d71c
SudoSavvy <donovanjheape@gmail.com>
2024-06-19 13:58:51 -0500

    struct and typedef

--- static_libraries/_putchar.c
+++ static_libraries/_putchar.c
@@ -1,6 +1,10 @@
 #include "main.h"
+#include "dog.h"
 
 int _putchar(char c)
 {
     return(0);
+}
+void init_dog(dog * d, char * name, float age, char * owner)
+{
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,12 +6,12 @@ typedef struct dog
     char *name;
     float age;
     char *owner;
-}
+}dog_t;
 
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct *dog);
-dog_h *new_dog(char *name, flaot age, char *owner);
-void free_dog(dog_h *d);
+dog_h *new_dog(char *name, float age, char *owner);
+void free_dog(struct dog *d);
 
 #endif
