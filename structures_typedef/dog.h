3db8146de1c52635896331abd43985f811eb17fa
SudoSavvy <donovanjheape@gmail.com>
2024-06-19 13:42:46 -0500

    struct and typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,9 +6,9 @@ typedef struct dog
     char *name;
     float age;
     char *owner;
-} dog_h
+}
 
-int _put har(char c);
+int _put chhar(char c);
 void init_dog)struct dog *d, char *name, float age, char *owner);
 void print_dog(struct* dog);
 dog_h *new_dog(char *name, flaot age, char *owner);
