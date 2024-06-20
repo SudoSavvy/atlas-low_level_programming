509781a8ad8097e1813360eb04991203df20df33
SudoSavvy <donovanjheape@gmail.com>
2024-06-20 15:21:49 -0500

    Back to functinos nad pointers <3

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - executes a function given as a parameter
+ *                  on each element of an array.
+ * @array: the array to iterate over
+ * @size: the size of the array
+ * @action: a pointer to the function to use
+ *
+ * Description: This function takes an array of integers, its size,
+ *              and a function pointer as parameters. It iterates
+ *              over each element of the array and applies the given
+ *              function to each element.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if (array != NULL && action != NULL)
+{
+for (size_t i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
+}
