72fc54d97d7768b3a01d38731c1abe7d38738c5c
SudoSavvy <donovanjheape@gmail.com>
2024-06-20 15:13:40 -0500

    Back to functinos nad pointers <3

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints a name using a given function
+ * @name: the name to print
+ * @f: function pointer to a function that takes a char * and returns void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+{
+f(name);
+}
+}
