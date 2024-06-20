680bb45739b5189cdc6dc2916fddf8aeb21aa063
SudoSavvy <donovanjheape@gmail.com>
2024-06-20 15:34:32 -0500

    Back to functinos nad pointers <3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: The sum of a and b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - Subtracts two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: The difference of a and b
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: The product of a and b
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - Divides two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: The result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
+}
+
+/**
+ * op_mod - Calculates the modulus of two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: The modulus of a and b
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
+}
