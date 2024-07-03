86f219e1d3a52679ef67dddd19423463237da055
SudoSavvy <donovanjheape@gmail.com>
2024-07-03 18:38:22 -0500

    Task 2 adding node end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -5,36 +5,36 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new_node, *current;
-size_t length = 0;
+    list_t *new_node, *current;
+    size_t length = 0;
 
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-return NULL;
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return NULL;
 
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
-return NULL;
-}
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return NULL;
+    }
 
-while (str[length])
-length++;
+    while (str[length] != '\0')
+        length++;
 
-new_node->next = NULL;
+    new_node->next = NULL;
 
-if (*head == NULL)
-*head = new_node;
-else
-{
-current = *head;
-while (current->next != NULL)
-current = current->next;
-current->next = new_node;
-}
+    if (*head == NULL)
+        *head = new_node;
+    else
+    {
+        current = *head;
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new_node;
+    }
 
-printf("[%lu] %s\n", (unsigned long)length, new_node->str);
+    printf("[%lu] %s\n", (unsigned long)length, new_node->str);
 
-return new_node;
+    return new_node;
 }
