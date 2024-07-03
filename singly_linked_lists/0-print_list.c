4dfa3ab1f807f9a884c9ea96fcf3e000c14bb868
SudoSavvy <donovanjheape@gmail.com>
2024-07-03 17:18:52 -0500

    Fixing issues with psuh and partially my stupidity <3

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,12 +15,13 @@ size_t print_list(const list_t *h)
     while (h != NULL)
     {
         if (h->str == NULL)
-            printf("[0] (nil)\n");
+            printf("%s/n", "[0] (nil)\n");
         else
-            printf("[%lu] %s\n", (unsigned long)strlen(h->str), h->str);
-
-        count++;
+            printf("[%lu] %s\n",h->len, h->str);
+        
         h = h->next;
+        count++;
+       
     }
 
     return (count);
