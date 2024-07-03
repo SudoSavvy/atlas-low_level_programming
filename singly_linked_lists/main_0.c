34ce3b06781b74acecf4728e27b399d9c2cb7d29
SudoSavvy <donovanjheape@gmail.com>
2024-07-03 17:24:33 -0500

    Fixing issues with psuh and partially my stupidity <3

--- singly_linked_lists/main_0.c
+++ singly_linked_lists/main_0.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - entry point of the program
- *
- * Return: 0 on success
- */
+* main - entry point of the program
+*
+* Return: 0 on success
+*/
 int main(void)
 {
-    list_t tail = {"School", 6, NULL};
-    list_t head = {"Best", 4, &tail};
+list_t tail = {"School", 6, NULL};
+list_t head = {"Best", 4, &tail};
 
-    size_t count = print_list(&head);
-    printf("Number of nodes: %lu\n", (unsigned long)count);
+size_t count = print_list(&head);
+printf("Number of nodes: %lu\n", (unsigned long)count);
 
-    return (0);
+return (0);
 }
