203df13beebd13b6fdd27b9a6724add5306c51da
SudoSavvy <donovanjheape@gmail.com>
2024-07-03 18:32:52 -0500

    Task 2 adding node end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,28 +4,33 @@
 #include "lists.h"
 
 /**
-* add_node_end - Adds a new node at the end of a list_t list.
-* @head: Pointer to the head pointer of the list.
-* @str: String to be duplicated and stored in the new node.
-*
-* Return: Address of the new element, or NULL if it failed.
-*/
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Pointer to the head pointer of the list.
+ * @str: String to be duplicated and stored in the new node.
+ *
+ * Return: Address of the new element, or NULL if it failed.
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new_node, *current;
+size_t length = 0;
 
 /* Allocate memory for new node */
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
-return NULL;
+return (NULL);
 
 /* Duplicate the string using strdup */
 new_node->str = strdup(str);
 if (new_node->str == NULL)
 {
 free(new_node);
-return NULL;
+return (NULL);
 }
+
+/* Calculate the length of the string */
+while (str[length])
+length++;
 
 new_node->next = NULL; /* Initialize next pointer of new node */
 
@@ -41,12 +46,12 @@ current = *head;
 while (current->next != NULL)
 current = current->next;
 
- /* Append the new node to the end of the list */
+/* Append the new node to the end of the list */
 current->next = new_node;
 }
 
-/*Print the length of the string and the string itself*/
-printf("[%lu] %s\n", strlen(new_node->str), new_node->str);
+/* Print the length of the string and the string itself */
+printf("[%lu] %s\n", (unsigned long)length, new_node->str);
 
-return new_node; /* Return the address of the new element */
+return (new_node); /* Return the address of the new element */
 }
