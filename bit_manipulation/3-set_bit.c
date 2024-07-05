343a1a01cfe7373cc5c46af41b186572a7b29fa9
SudoSavvy <donovanjheape@gmail.com>
2024-07-05 12:16:19 -0500

    fun bit manipulation <3

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -10,11 +10,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int) * 8)
-    {
-        return (-1); /*Index out of bounds*/
-    }
+if (index >= sizeof(unsigned long int) * 8)
+{
+return (-1); /*Index out of bounds*/
+}
 
-    *n |= (1UL << index); /*Set the bit at index to 1*/
-    return (1);
+*n |= (1UL << index); /*Set the bit at index to 1*/
+return (1);
 }
