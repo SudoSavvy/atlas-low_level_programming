ee6e3018f64a2555f2300fcf2b0726e98ad12173
SudoSavvy <donovanjheape@gmail.com>
2024-08-07 11:55:32 -0500

    Search Algorithm programming <3

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,12 +2,14 @@
 #include "search_algos.h"
 
 /**
-* linear_search - Searches for a value in an array of integers using the Linear search algorithm.
+* linear_search - Searches for a value in an array of integers using the
+* Linear search algorithm.
 * @array: Pointer to the first element of the array to search in.
 * @size: The number of elements in the array.
 * @value: The value to search for.
 *
-* Return: The first index where value is located, or -1 if value is not present or array is NULL.
+* Return: The first index where value is located, or -1 if value is not
+* present or array is NULL.
 */
 int linear_search(int *array, size_t size, int value)
 {
--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -22,12 +22,14 @@ printf("\n");
 }
 
 /**
-* binary_search - Searches for a value in a sorted array of integers using the Binary search algorithm.
+* binary_search - Searches for a value in a sorted array of integers using
+* the Binary search algorithm.
 * @array: Pointer to the first element of the array to search in.
 * @size: The number of elements in the array.
 * @value: The value to search for.
 *
-* Return: The index where value is located, or -1 if value is not present or array is NULL.
+* Return: The index where value is located, or -1 if value is not present
+* or array is NULL.
 */
 int binary_search(int *array, size_t size, int value)
 {
